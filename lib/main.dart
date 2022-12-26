ee1d5b6b6a2fa045330de84818dbce63d83360de
ChristopherLinnett <chrislinnett45389@gmail.com>
2022-12-26 17:45:08 +1000

    created starting project has basic screens and main provider files created

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:great_places/screens/places_list_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Great Places',
+      theme: ThemeData(
+        colorScheme:
+            const ColorScheme(primary: Colors.indigo, secondary: Colors.amber, error: Colors.red, brightness: Brightness.light, onPrimary: Colors.white, onSecondary: Colors.black, onError: Colors.blue, background: Colors.white, onBackground: Colors.black, surface: Colors.white, onSurface: Colors.black, ),
+      ),
+      home: const PlacesListScreen(),
+    );
+  }
+}
---
+++ lib/screens/places_list_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class PlacesListScreen extends StatelessWidget {
+  const PlacesListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Places'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Center(child: CircularProgressIndicator.adaptive(),)
+    );
+  }
+}
